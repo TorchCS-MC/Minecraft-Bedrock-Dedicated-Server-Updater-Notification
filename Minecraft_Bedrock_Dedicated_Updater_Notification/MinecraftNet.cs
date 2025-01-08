f1b7eb51ad4b92cf2873189e03df76d6a6f404f0
RealBytes86 <echtedeveloper@gmail.com>
2025-01-08 20:43:43 +0100

    upload

---
+++ Minecraft_Bedrock_Dedicated_Updater_Notification/MinecraftNet.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Minecraft_Bedrock_Dedicated_Updater_Notification
+{
+    public class BedrockDedicatedServer
+    {
+        public string Os { get; set; }
+        public string Type { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class MinecraftNet
+    {
+        private readonly HttpClient _httpClient = new();
+        private string _baseUrl;
+        private string _lang;
+
+        public event EventHandler<BedrockDedicatedServer> UpdateBedrockDedicatedServer;
+
+        private List<BedrockDedicatedServer> _lastKnownServers = new();
+
+        public MinecraftNet(string lang = "en-us")
+        {
+            _lang = lang;
+            _baseUrl = $"https://www.minecraft.net/{_lang}/";
+        }
+
+        public void SetLang(string lang)
+        {
+            _lang = lang;
+            _baseUrl = $"https://www.minecraft.net/{_lang}/";
+        }
+
+        public async Task<List<BedrockDedicatedServer>> GetBedrockDedicatedServerInfo()
+        {
+            try
+            {
+                string url = _baseUrl + "download/server/bedrock";
+                string htmlContent = await _httpClient.GetStringAsync(url);
+
+                string pattern = @"https:\/\/www\.minecraft\.net\/bedrockdedicatedserver\/bin-(win|linux)(-preview)?\/bedrock-server-(\d+\.\d+\.\d+\.\d+)\.zip";
+                var matches = Regex.Matches(htmlContent, pattern);
+
+                var servers = new List<BedrockDedicatedServer>();
+                foreach (Match match in matches)
+                {
+                    servers.Add(new BedrockDedicatedServer
+                    {
+                        Os = match.Groups[1].Value,
+                        Type = string.IsNullOrEmpty(match.Groups[2].Value) ? "stable" : "preview",
+                        Version = match.Groups[3].Value,
+                        Url = match.Value
+                    });
+                }
+
+                return servers;
+            }
+            catch
+            {
+                return new List<BedrockDedicatedServer>();
+            }
+        }
+
+        private async Task MonitorBedrockDedicatedServerUpdatesAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    List<BedrockDedicatedServer> currentServers = await GetBedrockDedicatedServerInfo();
+                    
+                    foreach (var server in currentServers)
+                    {
+                        if (!_lastKnownServers.Exists(s =>
+                                s.Version == server.Version &&
+                                s.Os == server.Os &&
+                                s.Type == server.Type))
+                        {
+                            UpdateBedrockDedicatedServer?.Invoke(this, server);
+                        }
+                    }
+
+                    _lastKnownServers = currentServers;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                await Task.Delay(3000);
+            }
+        }
+
+        public void InitEvents()
+        {
+            Task.Run(async () => await MonitorBedrockDedicatedServerUpdatesAsync());
+        }
+    }
+}
---
+++ Minecraft_Bedrock_Dedicated_Updater_Notification/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Discord.Webhook;
+using Discord;
+
+namespace Minecraft_Bedrock_Dedicated_Updater_Notification
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MainAsync().GetAwaiter().GetResult();
+        }
+        
+        private static string GetWebhookUrl()
+        {
+            string filePath = "config.txt";
+
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "webhook=<discord webhook>");
+                throw new FileNotFoundException($"Configuration file not found. A new file has been created at {filePath}. Please edit it and add your webhook URL.");
+            }
+
+            string fileContent = File.ReadAllText(filePath);
+            string pattern = @"webhook\s*=\s*(https?://[^\s]+)";
+            
+            Match match = Regex.Match(fileContent, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            throw new InvalidOperationException($"The configuration file does not contain a valid webhook URL. Please update the file at {filePath} with a valid webhook in the format: webhook=<discord webhook>");
+        }
+        private string CapitalizeFirstLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+
+        public async Task MainAsync()
+        {
+            MinecraftNet minecraftNet = new MinecraftNet();
+            minecraftNet.SetLang("de-de");
+            minecraftNet.UpdateBedrockDedicatedServer += OnBedrockDedicatedServerUpdate;
+            minecraftNet.InitEvents();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private async void OnBedrockDedicatedServerUpdate(object? sender, BedrockDedicatedServer e)
+        {
+            var webhookClient = new DiscordWebhookClient(GetWebhookUrl());
+
+            var color = e.Type.ToLower() == "stable" ? Color.Green : Color.Orange;
+            string platform;
+
+            if (e.Os == "win")
+            {
+                platform = "Windows";
+                
+            }  else if (e.Os == "linux")
+            {
+                platform = "Linux";
+            }
+            else
+            {
+                platform = e.Os;
+            }
+            
+            var embed = new EmbedBuilder()
+                .WithTitle($"New Bedrock Dedicated Server Update!")
+                .WithDescription($"A new version of the Bedrock Dedicated Server has been released.\n\n**Version:** {e.Version}\n**Operating System:** {platform}\n**Type:** {CapitalizeFirstLetter(e.Type)} ")
+                .WithColor(color)
+                .WithFooter(e.Version)
+                .WithTimestamp(DateTimeOffset.Now)
+                .Build();
+
+            var button = new ButtonBuilder()
+                .WithLabel("Download Now")
+                .WithStyle(ButtonStyle.Link)
+                .WithUrl(e.Url);
+
+            var component = new ComponentBuilder()
+                .WithButton(button)
+                .Build();
+
+            await webhookClient.SendMessageAsync(
+                text: null,
+                embeds: new[] { embed },
+                components: component
+            );
+        }
+    }
+}
